0e9b744224b048132613a6a1eab6a7c246fa5153
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2018-02-11 23:06:08 +0000

    remove scroll indicators

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -205,12 +205,13 @@
             }
         }
     }
-    if (browserOptions.horizontalscrollindicator) {
+	
+    /*if (browserOptions.horizontalscrollindicator) {
     	self.inAppBrowserViewController.webView.scrollView.showsHorizontalScrollIndicator = browserOptions.horizontalscrollindicator ? YES : NO;
     }
 	if(browserOptions.verticalscrollindicator){
 		self.inAppBrowserViewController.webView.scrollView.showsVerticalScrollIndicator = browserOptions.verticalscrollindicator ? YES : NO;
-	}
+	}*/
 
     [self.inAppBrowserViewController navigateTo:url];
     [self show:nil withNoAnimate:browserOptions.hidden];
